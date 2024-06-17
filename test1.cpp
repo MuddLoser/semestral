f901ed3a63c5ff6bc07ab89413c09226c6f0c7fe
mudd <matiurreamoya@hotmail.com>
2024-06-17 00:56:38 -0400

    me rindo

--- test1.cpp
+++ test1.cpp
@@ -3,6 +3,8 @@
 //hay que arreglar que lea strings en vez de un arreglo de letras
 //y cambiar el nombre de variables
 
+//Falta la codificiacion con el trie
+
 #include <iostream>
 #include <queue>
 using namespace std;
@@ -22,14 +24,14 @@ public:
 	}
 };
 
-class Compare {
+class Compara {
 public:
 	bool operator()(HuffmanTreeNode* a, HuffmanTreeNode* b){
 		return a->freq > b->freq;
 	}
 };
 
-HuffmanTreeNode* generateTree(priority_queue<HuffmanTreeNode*, vector<HuffmanTreeNode*>, Compare> pq){
+HuffmanTreeNode* generateTree(priority_queue<HuffmanTreeNode*, vector<HuffmanTreeNode*>, Compara> pq){
 
 	while (pq.size() != 1) {
 		HuffmanTreeNode* left = pq.top();
@@ -40,53 +42,30 @@ HuffmanTreeNode* generateTree(priority_queue<HuffmanTreeNode*, vector<HuffmanTre
 
 		pq.pop();
 
-		// A new node is formed
-		// with frequency left->freq
-		// + right->freq
-
-		// We take data as '$'
-		// because we are only
-		// concerned with the
-		// frequency
-		HuffmanTreeNode* node = new HuffmanTreeNode('$', left->freq + right->freq);
+		HuffmanTreeNode* node = new HuffmanTreeNode('x', left->freq + right->freq);
 		node->left = left;
 		node->right = right;
 
-		// Push back node
-		// created to the
-		// Priority Queue
 		pq.push(node);
 	}
 
 	return pq.top();
 }
 
-// Function to print the
-// huffman code for each
-// character.
 
-// It uses arr to store the codes
-void printCodes(HuffmanTreeNode* root, int arr[], int top)
-{
-	// Assign 0 to the left node
-	// and recur
+
+void printCodes(HuffmanTreeNode* root, int arr[], int top){
+
 	if (root->left) {
 		arr[top] = 0;
 		printCodes(root->left, arr, top + 1);
 	}
 
-	// Assign 1 to the right
-	// node and recur
 	if (root->right) {
 		arr[top] = 1;
 		printCodes(root->right, arr, top + 1);
 	}
 
-	// If this is a leaf node,
-	// then we print root->data
-
-	// We also print the code
-	// for this character from arr
 	if (!root->left && !root->right) {
 		cout << root->data << " ";
 		for (int i = 0; i < top; i++) {
@@ -98,32 +77,42 @@ void printCodes(HuffmanTreeNode* root, int arr[], int top)
 
 void HuffmanCodes(char data[], int freq[], int size){
 
-	// Declaring priority queue
-	// using custom comparator
-	priority_queue<HuffmanTreeNode*,
-				vector<HuffmanTreeNode*>,
-				Compare>
-		pq;
+	priority_queue<HuffmanTreeNode*, vector<HuffmanTreeNode*>, Compara> pq;
 
-	// Populating the priority
-	// queue
 	for (int i = 0; i < size; i++) {
 		HuffmanTreeNode* newNode = new HuffmanTreeNode(data[i], freq[i]);
 		pq.push(newNode);
 	}
 
-	// Generate Huffman Encoding
-	// Tree and get the root node
 	HuffmanTreeNode* root = generateTree(pq);
 
-	// Print Huffman Codes
 	int arr[MAX_SIZE], top = 0;
 	printCodes(root, arr, top);
 }
 
-// Driver Code
-int main()
-{
+int main(){
+	/*vector<HuffmanTreeNode> data1;
+	HuffmanTreeNode *aux;
+	string a = "tangananica-tanganana";
+	bool check[94]; //true si el char esta en el string
+					//false si no está
+	vector<char> aux1;
+	vector<int>  aux2;
+
+
+	for (int i = 0; i<a.size(); i++){
+		int B = a[i] - 32;
+		int x = 0;
+		if(!check[B]){			//no está
+			check[B] = true;
+			aux1.push_back(a[i]);
+			x = 1;
+		}else if(check[B]){		//está!
+			x = x + 1;
+		}if(i=a.si)
+	}
+*/
+
 	char data[] = { 'a', 'b', 'c', 'd', 'e', 'f' };
 	int freq[] = { 5, 9, 12, 13, 16, 45 };
 	int size = sizeof(data) / sizeof(data[0]);
